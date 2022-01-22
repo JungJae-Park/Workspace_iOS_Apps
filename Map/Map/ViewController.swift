db6bb7b4293bd406e175a02e51c6992f4c63c2a2
JJPark9411 <JJPark9411@gmail.com>
2022-01-22 17:16:57 +0900

    Add Segment Control, Map Kit View, and Labels

---
+++ Map/Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Park's MacBook Pro on 2022/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
