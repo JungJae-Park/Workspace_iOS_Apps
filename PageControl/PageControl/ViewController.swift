45d5453ed77b3802a68daf7d2abba0d8f30e966b
JJPark9411 <JJPark9411@gmail.com>
2022-01-23 16:01:18 +0900

    Improve pageChange function to show  images when page changed

--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
+
 class ViewController: UIViewController {
 
     @IBOutlet var imgView: UIImageView!
@@ -15,9 +17,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.white
+        
+        imgView.image = UIImage(named: images[0])
     }
 
     @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
     }
 }
 
