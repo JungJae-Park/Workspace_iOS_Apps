3772d4cfde4e7175cb33d4c95e486e196ae2840d
JJPark9411 <JJPark9411@gmail.com>
2022-01-30 20:41:04 +0900

    Add labels, progress view, buttons and slider

---
+++ Audio/Audio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Audio
+//
+//  Created by Park's MacBook Pro on 2022/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
