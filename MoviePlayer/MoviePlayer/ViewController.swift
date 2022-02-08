d56510ddde919d2ee2615c144894f1566d5aad8e
JJPark9411 <JJPark9411@gmail.com>
2022-02-08 23:31:16 +0900

    Add labels and buttons with image

--- MoviePlayer/MoviePlayer/ViewController.swift
+++ MoviePlayer/MoviePlayer/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // 이미지 사이즈 조절
+    func resizeImage(image: UIImage, width: CGFloat, height: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
 
 
 }
