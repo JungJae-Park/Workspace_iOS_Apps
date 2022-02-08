1dc8c5a8cdf6026f7a655850cc0cb37e7afbb84a
JJPark9411 <JJPark9411@gmail.com>
2022-02-08 22:38:09 +0900

    Add media resources

---
+++ MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by Park's MacBook Pro on 2022/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
